713204267551fc32b23f2d101a2f1aa76b979c68
MeiirimZh <zh.meyirim@gmail.com>
2026-02-16 08:53:41 +0500

    Update: Register.tsx

--- src/screens/auth/Register.tsx
+++ src/screens/auth/Register.tsx
@@ -1,51 +1,126 @@
-// src/screens/Register.tsx
-import React, { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import { useState } from "react";
+
+import { View, TextInput, TouchableOpacity, StyleSheet, Alert } from "react-native";
+import AppText from "../../../components/AppText";
+
 import { useAuth } from "../../context/AuthContext";
 
-export default function Register() {
-  const { register } = useAuth();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+import { StackScreenProps } from "@react-navigation/stack";
+import { AuthStackParamList } from "../../navigation/types";
 
-  const handleRegister = async () => {
-    setLoading(true);
-    try {
-      await register(email, password);
-      Alert.alert("Регистрация успешна", "Теперь вы можете войти в приложение");
-    } catch (err: any) {
-      Alert.alert("Ошибка регистрации", err.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+import { theme } from "../../theme";
+import LogoIcon from "../../../assets/svg/LogoIcon";
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Регистрация</Text>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        style={styles.input}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <TextInput
-        placeholder="Пароль"
-        value={password}
-        onChangeText={setPassword}
-        style={styles.input}
-        secureTextEntry
-      />
-      <Button title={loading ? "Загрузка..." : "Зарегистрироваться"} onPress={handleRegister} disabled={loading} />
-    </View>
+type Props = StackScreenProps<AuthStackParamList, "Register">;
+
+export default function Register({ navigation }: Props) {
+    const { register } = useAuth();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleRegister = async () => {
+        setLoading(true);
+        try {
+            await register(email, password);
+        }
+        catch (error: any) {
+            Alert.alert("Ошибка регистрации", error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <View style={ styles.main }>
+            <View style={ styles.contentWrapper }>
+                <LogoIcon
+                    width={ 100 }
+                    height={ 100 }
+                    style={ styles.logo } />
+                <View style={ styles.content }>
+                    <AppText style={{ fontSize: 24, alignSelf: 'center', color: theme.colors.text }}>Регистрация</AppText>
+                    <View style={ styles.form }>
+                        <TextInput
+                            placeholder="Email"
+                            value={ email }
+                            onChangeText={ setEmail }
+                            style={ [styles.textInput, styles.shadow] }
+                            keyboardType="email-address"
+                            autoCapitalize="none" />
+                        <TextInput
+                            placeholder="Пароль"
+                            value={ password }
+                            onChangeText={ setPassword }
+                            style={ [styles.textInput, styles.shadow] }
+                            secureTextEntry />
+                        <TouchableOpacity
+                            style={ [styles.submitButton, styles.shadow] }
+                            onPress={ handleRegister } >
+                            <AppText style={{ color: theme.colors.onPrimary }}>Зарегистрироваться</AppText>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <TouchableOpacity
+                            style={{ alignItems: 'center', width: '100%', padding: theme.spacing.md }} 
+                            onPress={() => navigation.navigate("Login")}>
+                            <AppText style={{ color: theme.colors.textMuted }}>Уже есть аккаунт?</AppText>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", padding: 20 },
-  title: { fontSize: 24, fontWeight: "bold", marginBottom: 20, textAlign: "center" },
-  input: { borderWidth: 1, borderColor: "#ccc", borderRadius: 8, padding: 10, marginBottom: 15 },
+    main: {
+        flex: 1,
+        justifyContent: 'center',
+
+        padding: 50,
+
+        backgroundColor: theme.colors.bg
+    },
+    contentWrapper: {
+        position: 'relative'
+    },
+    content: {
+        gap: theme.spacing.lg,
+    },
+
+    form: {
+        gap: theme.spacing.md
+    },
+    logo: {
+        position: 'absolute',
+        top: -150,
+
+        alignSelf: 'center'
+    },
+
+    textInput: {
+        backgroundColor: theme.colors.bgLight,
+
+        borderRadius: 10,
+
+        padding: theme.spacing.md
+    },
+    submitButton: {
+        alignItems: 'center',
+
+        backgroundColor: theme.colors.primary,
+
+        borderRadius: 10,
+
+        padding: theme.spacing.md
+    },
+
+    shadow: {
+        elevation: 5,
+        shadowColor: theme.colors.shadow,
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.12,
+        shadowRadius: 10,
+    }
 });
