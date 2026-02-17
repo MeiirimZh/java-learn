a77a5596e510b2470071510413e2eb26879ce3cb
MeiirimZh <zh.meyirim@gmail.com>
2026-02-17 10:09:53 +0500

    save user's passed tests

--- src/screens/main/Tests.tsx
+++ src/screens/main/Tests.tsx
@@ -1,12 +1,47 @@
-import { StyleSheet, View, FlatList, Linking } from "react-native";
+import { useState, useEffect } from "react";
+
+import { StyleSheet, View, FlatList, Linking, ActivityIndicator } from "react-native";
 import TestCard from "../../../components/TestCard";
 
 import useTests from "../../../src/hooks/useTests";
 
 import { theme } from "../../../src/theme";
 
+import { useAuth } from "../../context/AuthContext";
+import { doc, updateDoc, arrayUnion, onSnapshot } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+
 export default function Tests() {
     const { tests } = useTests();
+
+    const [ passedTests, setPassedTests ] = useState<number[]>([]);
+    const [ loading, setLoading ] = useState<boolean>(true);
+
+    const { user } = useAuth();
+
+    useEffect(() => {
+        if (!user) return;
+
+        const unsubscribe = onSnapshot(
+            doc(db, "users", user.uid),
+            (docSnap) => {
+                if (docSnap.exists()) {
+                    setPassedTests(docSnap.data().passedTests ?? []);
+                }
+                setLoading(false);
+            }
+        );
+    
+        return unsubscribe;
+    }, [user]);
+    
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" color={ theme.colors.primary } />
+            </View>
+        )
+    }
 
     return (
         <View style={ styles.main }>
@@ -15,8 +50,20 @@ export default function Tests() {
             renderItem={({ item }) => (
                 <TestCard
                     title={ item.title }
-                    isCompleted={ false }
-                    onPress={() => Linking.openURL(item.link)} />
+                    isCompleted={ passedTests.includes(item.id) }
+                    onPress={() => {
+                        const updateUser = async () => {
+                            if (!user) return;
+
+                            await updateDoc(doc(db, "users", user.uid), {
+                                passedTests: arrayUnion(item.id)
+                            });
+                        };
+
+                        updateUser();
+
+                        Linking.openURL(item.link)
+                    }} />
             )}
             showsVerticalScrollIndicator={ false }
             ItemSeparatorComponent={() => (
