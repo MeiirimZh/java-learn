e45f7372392bd987f68fef66f39a94bd655d5a6e
MeiirimZh <zh.meyirim@gmail.com>
2026-02-18 09:47:09 +0500

    updated tests in materials

--- assets/materials/tests.ts
+++ assets/materials/tests.ts
@@ -1,12 +1,6 @@
 export const tests = [
-    {id: 1, title: 'Lorem ipsum', link: 'https://www.google.com/'},
-    {id: 2, title: 'Dolor sit', link: 'https://reactnative.dev/docs/getting-started'},
-    {id: 3, title: 'Amet consectetur', link: 'https://reactnavigation.org/docs/getting-started/'},
-    {id: 4, title: 'Lorem ipsum', link: 'https://ionic.io/ionicons'},
-    {id: 5, title: 'Dolor sit', link: 'https://www.google.com/'},
-    {id: 6, title: 'Lorem ipsum', link: 'https://www.google.com/'},
-    {id: 7, title: 'Amet consectetur', link: 'https://www.google.com/'},
-    {id: 8, title: 'Dolor sit', link: 'https://www.google.com/'},
-    {id: 9, title: 'Dolor sit', link: 'https://www.google.com/'},
-    {id: 10, title: 'Dolor sit', link: 'https://www.google.com/'},
+    {id: 1, title: 'Условные операторы', link: 'https://docs.google.com/forms/d/e/1FAIpQLSf_AHyn1IxUxTc8nWB3PpB4X1c1HH5XuLHBOToRu-NfUVcSmQ/closedform'},
+    {id: 2, title: 'Циклы ', link: 'https://docs.google.com/forms/d/e/1FAIpQLScXhEc53czKgfcSy7g7rLuCKuXAphc6HCD2UB79HXanIJgORw/closedform'},
+    {id: 3, title: 'Цикл While', link: 'https://docs.google.com/forms/d/e/1FAIpQLSdaTmdMgWmb551xXehLOPDYU3DIsS54eZDjDIF5SBcx44i2mA/closedform'},
+    {id: 4, title: 'Массивы', link: 'https://docs.google.com/forms/d/e/1FAIpQLSf39GrpL8OBPFsofkKEiWMfCHIf_g8Alk1HgQbEw9wQMTAy_A/closedform'},
 ];
